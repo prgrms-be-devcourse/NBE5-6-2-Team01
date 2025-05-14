68046d4cc97814507efdb215133c34f61c492769
Park Hyundo <hyundo13@naver.com>
2025-05-14 12:08:34 +0900

    feat: 모임 생성 시 모임명, 목적 필수 입력하는 기능 구현

--- src/main/java/com/grepp/synapse4/app/controller/web/meeting/payload/MeetingRegistRequest.java
+++ src/main/java/com/grepp/synapse4/app/controller/web/meeting/payload/MeetingRegistRequest.java
@@ -3,17 +3,18 @@ package com.grepp.synapse4.app.controller.web.meeting.payload;
 import com.grepp.synapse4.app.model.meeting.code.Purpose;
 import com.grepp.synapse4.app.model.meeting.dto.MeetingDto;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class MeetingRegistRequest {
 
-  @NotBlank(message = "모임 이름은 필수입니다.")
+  @NotBlank(message = "모임명을 필수로 입력해야 합니다.")
   private String title;
 
-  @NotBlank(message = "모임 설명은 필수입니다.")
   private String description;
 
+  @NotNull(message = "목적을 필수로 입력해야 합니다.")
   private Purpose purpose;
 
   private Boolean isDutch;
