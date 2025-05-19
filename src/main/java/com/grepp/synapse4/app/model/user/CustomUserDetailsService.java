8e23f852b036b70274b2a8bf35ad1b67157b1ad8
Park Hyundo <hyundo13@naver.com>
2025-05-19 13:05:14 +0900

    refactor: userId 불러오는 로직 변경

--- src/main/java/com/grepp/synapse4/app/model/user/CustomUserDetailsService.java
+++ src/main/java/com/grepp/synapse4/app/model/user/CustomUserDetailsService.java
@@ -5,6 +5,8 @@ import com.grepp.synapse4.app.model.user.entity.User;
 import com.grepp.synapse4.app.model.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -27,8 +29,9 @@ public class CustomUserDetailsService implements UserDetailsService {
         return new CustomUserDetails(user);
     }
 
-    public Long loadUserIdByAccount(String account) throws UsernameNotFoundException{
-        User user = userRepository.findByUserAccount(account)
+    public Long loadUserIdByAccount() throws UsernameNotFoundException{
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByUserAccount(authentication.getName())
             .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
         return user.getId();
     }
